e7145bcbab9bd5750d50c44ed1406fecb3a32165
uyenlex <21020672@vnu.edu.vn>
2023-10-11 23:48:24 +0700

    landing page: navbar, slider

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import NavigationBar from "./components/NavigationBar";
+import Slider from "./components/Slider";
+
+function App() {
+  return (
+    <div>
+      <NavigationBar />
+      <Slider />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Slider.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Slider = () => {
+  return (
+    <section>
+        <img className='w-full' src="https://cdn.store-assets.com/s/402514/f/11242244.png" alt="cycle7" />
+    </section>
+  )
+}
+
+export default Slider
