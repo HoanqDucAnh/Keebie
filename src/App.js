d3e876273ff4eb8ae97b9a36acf298702787e65c
uyenlex <21020672@vnu.edu.vn>
2023-10-12 23:45:23 +0700

    carousel

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-import React from "react";
+import React, { useEffect } from "react";
 import NavigationBar from "./components/NavigationBar";
-import Slider from "./components/Slider";
+import Carousel from "./components/Carousel";
 
 function App() {
+  useEffect(() => {
+    document.title = "Keebi3."
+  }, []);
+
   return (
     <div>
       <NavigationBar />
-      <Slider />
+      <Carousel />
     </div>
   );
 }
