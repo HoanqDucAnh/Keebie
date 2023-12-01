a4226fe92921918ae1b53df19fedf4f1251f1530
Kaasw <hieu1282003@gmail.com>
2023-12-01 18:07:30 +0700

    Auto create user table

--- backend/main.py
+++ backend/main.py
@@ -13,3 +13,4 @@ if __name__ == "__main__":
     import uvicorn
 
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    # uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
