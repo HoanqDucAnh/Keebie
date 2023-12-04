046dad192f9f81527b4188ef8f5b9ba4595e9156
uyenlex <21020672@vnu.edu.vn>
2023-12-04 21:56:48 +0700

    backend: Unset `reload=True` * Bad for production

--- backend/main.py
+++ backend/main.py
@@ -37,5 +37,5 @@ app.mount("/api", api_router, name="api")
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000)
     # uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
