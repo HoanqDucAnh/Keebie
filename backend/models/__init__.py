259d5f1a6fa8d80451c318eaa7a0c9154265d8ca
quanandon <tranngocanhquan03@gmail.com>
2023-12-30 18:05:37 +0700

    Change some files

--- backend/models/__init__.py
+++ backend/models/__init__.py
@@ -4,7 +4,4 @@ from .product import Product
 from .product import Category
 from .product import ProductImage
 from .order import Order, OrderDetail
-from .review import Review
-from .sale import Sale, SaleDetail
-from .voucher import Voucher, VoucherCustomer
 from .verify import Verify
--- backend/schemas/__init__.py
+++ backend/schemas/__init__.py
@@ -3,6 +3,3 @@ from .auth import *
 from .product import *
 from .order import *
 from .verify import *
-from .sale import *
-from .review import *
-from .voucher import *
