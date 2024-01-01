63725b7209cde6e70d4e2f13e6ec17936fab3405
Kaasw <hieu1282003@gmail.com>
2024-01-01 17:54:21 +0700

    Search screen remove

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ import ProductDetailScreen from "./components/shared/productComponent/ProductDet
 import ProductPageScreen from "./screen/ProductPageScreen";
 import CartScreen from "./screen/CartScreen";
 import PaymentScreen from "./screen/PaymentScreen";
-import SearchScreen from "./screen/SearchScreen";
+
 
 function App() {
 	useEffect(() => {
@@ -57,9 +57,6 @@ function App() {
 					</Route>
 					<Route path="/404">
 						<NotFoundScreen />
-					</Route>
-					<Route path="/search">
-						<SearchScreen />
 					</Route>
 					<Route exact path="/">
 						<HomeScreen />
